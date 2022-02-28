264c9bd9dd8291e4466b73815265fb2a5da6a2a5
maxbildner <39280058+maxbildner@users.noreply.github.com>
2022-02-28 13:37:36 -0500

    add images api

---
+++ amplify/backend/function/GetImagesHandler/src/index.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+exports.handler = async (event) => {
+  console.log(`EVENT: ${JSON.stringify(event)}`); // logs to cloudwatch where you can see logs
+  // event.pathParameters.customerId
+
+  return {
+    statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+    },
+    body: JSON.stringify("Hello from Lambda!"),
+  };
+};
